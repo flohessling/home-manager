96d052b532bb4f4c6d2de495039c7251d59317ef
Flo Hessling <f.hessling@shopware.com>
2022-09-15 13:57:06 +0200

    update lvim config and add itermcolors

--- home/lvim/config.lua
+++ home/lvim/config.lua
@@ -1,5 +1,7 @@
 -- colorscheme
 lvim.colorscheme = "vscode"
+-- lvim.colorscheme = "tokyonight"
+-- vim.g.tokyonight_style = "night"
 
 -- general
 lvim.log.level = "warn"
@@ -19,21 +21,21 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["x"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+    name = "+Trouble",
+    r = { "<cmd>Trouble lsp_references<cr>", "References" },
+    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+    d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Test",
-  t = { "<cmd>TestNearest<cr>", "Nearest" },
-  T = { "<cmd>TestFile<cr>", "File" },
-  a = { "<cmd>TestSuite<cr>", "Suite" },
-  l = { "<cmd>TestLast<cr>", "Last" },
-  g = { "<cmd>TestVisit<cr>", "Visit" },
+    name = "+Test",
+    t = { "<cmd>TestNearest<cr>", "Nearest" },
+    T = { "<cmd>TestFile<cr>", "File" },
+    a = { "<cmd>TestSuite<cr>", "Suite" },
+    l = { "<cmd>TestLast<cr>", "Last" },
+    g = { "<cmd>TestVisit<cr>", "Visit" },
 }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -46,26 +48,27 @@ lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "yaml",
-  "php",
-  "go",
-  "hcl",
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "yaml",
+    "php",
+    "go",
+    "hcl",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 lvim.lsp.automatic_servers_installation = true
+-- lvim.lsp.installer.setup.automatic_installation = true
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
@@ -97,47 +100,47 @@ formatters.setup {
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  -- go
-  { name = "golangci_lint" },
+    -- go
+    -- { name = "golangci_lint" },
 
-  -- php
-  { name = "php" },
-  { name = "phpstan" },
-  { name = "psalm" },
+    -- php
+    { name = "php" },
+    { name = "phpstan" },
 }
 
 -- Additional Plugins
 lvim.plugins = {
-  { "Mofiqul/vscode.nvim" },
-  { "vim-test/vim-test" },
-  { "nelsyeung/twig.vim" },
-  { "folke/trouble.nvim" },
+    { "Mofiqul/vscode.nvim" },
+    { "vim-test/vim-test" },
+    { "nelsyeung/twig.vim" },
+    { "folke/trouble.nvim" },
+    { "folke/tokyonight.nvim" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- Go: auto-import on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.go" },
-  callback = function()
-    vim.lsp.buf.formatting_sync(nil, 3000)
-  end,
+    pattern = { "*.go" },
+    callback = function()
+        vim.lsp.buf.formatting_sync(nil, 3000)
+    end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.go" },
-  callback = function()
-    local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-    params.context = { only = { "source.organizeImports" } }
+    pattern = { "*.go" },
+    callback = function()
+        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+        params.context = { only = { "source.organizeImports" } }
 
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    for _, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-        else
-          vim.lsp.buf.execute_command(r.command)
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+                else
+                    vim.lsp.buf.execute_command(r.command)
+                end
+            end
         end
-      end
-    end
-  end,
+    end,
 })
