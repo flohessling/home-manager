0b820171bd230d4ad59e8c821cd46718938ba7ff
Flo Hessling <f.hessling@shopware.com>
2023-04-12 15:09:38 +0200

    update nvim vscode colorscheme plugin

--- home/nvim/lua/plugins/vscode.lua
+++ home/nvim/lua/plugins/vscode.lua
@@ -1,19 +1,17 @@
-local M = {
-    "Mofiqul/vscode.nvim",
-    enabled = true,
-    lazy = false,
-    priority = 999,
+return {
+    {
+        "Mofiqul/vscode.nvim",
+        enabled = true,
+        lazy = false,
+        priority = 999,
+        config = function()
+            require("vscode.colors").get_colors()
+            require("vscode").setup({
+                transparent = true, -- enable transparent background
+                italic_comments = true, -- enable italic comments
+                disable_nvimtree_bg = true, -- disable nvim-tree background color
+            })
+            vim.cmd.colorscheme("vscode")
+        end,
+    },
 }
-
-function M.config()
-    local c = require("vscode.colors")
-    local vscode = require("vscode")
-    vscode.setup({
-        transparent = true, -- enable transparent background
-        italic_comments = true, -- enable italic comments
-        disable_nvimtree_bg = true, -- disable nvim-tree background color
-    })
-    vim.cmd("colorscheme vscode")
-end
-
-return M
