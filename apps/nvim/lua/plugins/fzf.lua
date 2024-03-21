a4cbcbe03a3337749e10703f246527108ab5d39c
Flo Hessling <f.hessling@shopware.com>
2024-03-21 14:55:58 +0100

    switch from fzf to telescope

--- apps/nvim/lua/plugins/fzf.lua
+++ apps/nvim/lua/plugins/fzf.lua
@@ -19,38 +19,38 @@ return {
 				},
 			})
 		end,
-		keys = {
-			{ "<C-e>", "<cmd>FzfLua oldfiles<CR>", { desc = "[?] Find recently opened files" } },
-			{ "<leader><space>", "<cmd>FzfLua buffers<CR>", { desc = "[ ] Find existing buffers" } },
-
-			{ "<leader>hk", "<cmd>FzfLua keymaps<CR>", { desc = "Search [H]ot[K]eys" } },
-			{ "<leader>f", "<cmd>FzfLua files<CR>", { desc = "Search [F]iles" } },
-			{ "<leader>sf", "<cmd>FzfLua files<CR>", { desc = "[S]earch [F]iles" } },
-			{ "<leader>sh", "<cmd>FzfLua help_tags<CR>", { desc = "[S]earch [H]elp" } },
-			{ "<leader>sg", "<cmd>FzfLua live_grep<CR>", { desc = "[S]earch by [G]rep" } },
-			{ "<leader>sd", "<cmd>FzfLua diagnostics_document<CR>", { desc = "[S]earch [D]iagnostics" } },
-			{ "<leader>sl", "<cmd>FzfLua resume<CR>", { desc = "[S]earch [L]ast (resume)" } },
-			{ "<leader>sr", "<cmd>FzfLua resume<CR>", { desc = "[S]earch [R]esume" } },
-			{
-				"<leader>P",
-				function()
-					local fzf_lua = require("fzf-lua")
-
-					fzf_lua.fzf_exec(require("project_nvim").get_recent_projects(), {
-						prompt = "Projects> ",
-						exec_empty_query = true,
-						fn_transform = function(x)
-							return fzf_lua.utils.ansi_codes.magenta(x)
-						end,
-						actions = {
-							["default"] = function(selected)
-								fzf_lua.files({ cwd = selected[1] })
-							end,
-						},
-					})
-				end,
-				{ desc = "[S]earch [P]rojects" },
-			},
-		},
+		-- keys = {
+		-- 	{ "<C-e>", "<cmd>FzfLua oldfiles<CR>", { desc = "[?] Find recently opened files" } },
+		-- 	{ "<leader><space>", "<cmd>FzfLua buffers<CR>", { desc = "[ ] Find existing buffers" } },
+		--
+		-- 	{ "<leader>hk", "<cmd>FzfLua keymaps<CR>", { desc = "Search [H]ot[K]eys" } },
+		-- 	{ "<leader>f", "<cmd>FzfLua files<CR>", { desc = "Search [F]iles" } },
+		-- 	{ "<leader>sf", "<cmd>FzfLua files<CR>", { desc = "[S]earch [F]iles" } },
+		-- 	{ "<leader>sh", "<cmd>FzfLua help_tags<CR>", { desc = "[S]earch [H]elp" } },
+		-- 	{ "<leader>sg", "<cmd>FzfLua live_grep<CR>", { desc = "[S]earch by [G]rep" } },
+		-- 	{ "<leader>sd", "<cmd>FzfLua diagnostics_document<CR>", { desc = "[S]earch [D]iagnostics" } },
+		-- 	{ "<leader>sl", "<cmd>FzfLua resume<CR>", { desc = "[S]earch [L]ast (resume)" } },
+		-- 	{ "<leader>sr", "<cmd>FzfLua resume<CR>", { desc = "[S]earch [R]esume" } },
+		-- 	{
+		-- 		"<leader>P",
+		-- 		function()
+		-- 			local fzf_lua = require("fzf-lua")
+		--
+		-- 			fzf_lua.fzf_exec(require("project_nvim").get_recent_projects(), {
+		-- 				prompt = "Projects> ",
+		-- 				exec_empty_query = true,
+		-- 				fn_transform = function(x)
+		-- 					return fzf_lua.utils.ansi_codes.magenta(x)
+		-- 				end,
+		-- 				actions = {
+		-- 					["default"] = function(selected)
+		-- 						fzf_lua.files({ cwd = selected[1] })
+		-- 					end,
+		-- 				},
+		-- 			})
+		-- 		end,
+		-- 		{ desc = "[S]earch [P]rojects" },
+		-- 	},
+		-- },
 	},
 }
--- apps/nvim/lua/plugins/mini.lua
+++ apps/nvim/lua/plugins/mini.lua
@@ -1,48 +1,49 @@
 return {
-    "echasnovski/mini.nvim",
-    version = false,
-    lazy = false,
-    init = function()
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-            callback = function()
-                vim.b.miniindentscope_disable = true
-            end,
-        })
-    end,
+	"echasnovski/mini.nvim",
+	version = false,
+	lazy = false,
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+	end,
 
-    config = function()
-        require("mini.move").setup()
-        require("mini.bracketed").setup()
-        require("mini.surround").setup()
-        require("mini.trailspace").setup()
-        require("mini.bufremove").setup()
-        require("mini.cursorword").setup()
-        require("mini.misc").setup({ make_global = { "setup_auto_root" } })
-        require("mini.misc").setup_auto_root()
+	config = function()
+		require("mini.move").setup()
+		require("mini.bracketed").setup()
+		require("mini.surround").setup()
+		require("mini.trailspace").setup()
+		require("mini.bufremove").setup()
+		require("mini.cursorword").setup()
+		require("mini.misc").setup({ make_global = { "setup_auto_root" } })
+		require("mini.misc").setup_auto_root()
 
-        require("mini.indentscope").setup({
-            -- symbol = "▏",
-            symbol = "│",
-            options = { try_as_border = true },
-            draw = {
-                animation = require("mini.indentscope").gen_animation.none(),
-            },
-        })
+		require("mini.indentscope").setup({
+			-- symbol = "▏",
+			-- symbol = "│",
+			symbol = "╎",
+			options = { try_as_border = true },
+			draw = {
+				animation = require("mini.indentscope").gen_animation.none(),
+			},
+		})
 
-        require("mini.comment").setup({
-            options = {
-                custom_commentstring = function()
-                    return require("ts_context_commentstring.internal").calculate_commentstring()
-                        or vim.bo.commentstring
-                end,
-            },
-            mappings = {
-                comment_line = "<leader>/",
-                comment_visual = "<leader>/",
-            },
-        })
-    end,
+		require("mini.comment").setup({
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+			mappings = {
+				comment_line = "<leader>/",
+				comment_visual = "<leader>/",
+			},
+		})
+	end,
     -- stylua: ignore
     keys = {
         { "<leader>X", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer", },
