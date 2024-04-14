59161f0cba70699026ed36579109a858d102c47d
Flo Hessling <f.hessling@shopware.com>
2024-04-14 14:09:31 +0200

    format tf and tg update scripts

--- apps/zsh/update-tf.sh
+++ apps/zsh/update-tf.sh
@@ -13,15 +13,15 @@ VERSION=$1
 
 mkdir -p ~/.local/bin/tf-update
 
-wget -O ~/.local/bin/tf-update/tf_$VERSION.zip "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_darwin_arm64.zip" 
+wget -O ~/.local/bin/tf-update/tf_"$VERSION".zip "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_darwin_arm64.zip"
 
 cd ~/.local/bin/tf-update
-unzip tf_$VERSION.zip 
+unzip tf_"$VERSION".zip
 
 OLDVERSION=$(terraform -v | awk '{ print $2 }' | head -1)
 
 cd ~/.local/bin
-mv terraform terraform_${OLDVERSION:1}
+mv terraform terraform_"${OLDVERSION:1}"
 mv tf-update/terraform .
 
 echo "updated $(pwd)/terraform to version $VERSION"
--- apps/zsh/update-tg.sh
+++ apps/zsh/update-tg.sh
@@ -13,15 +13,15 @@ VERSION=$1
 
 mkdir -p ~/.local/bin/tg-update
 
-wget -O ~/.local/bin/tg-update/tg_$VERSION "https://github.com/gruntwork-io/terragrunt/releases/download/${VERSION}/terragrunt_darwin_arm64" 
+wget -O ~/.local/bin/tg-update/tg_"$VERSION" "https://github.com/gruntwork-io/terragrunt/releases/download/${VERSION}/terragrunt_darwin_arm64"
 
 cd ~/.local/bin/tg-update
 
 OLDVERSION=$(terragrunt -v | awk '{ print $3 }')
 
 cd ~/.local/bin
-mv terragrunt terragrunt_${OLDVERSION:1}
-mv tg-update/tg_$VERSION ~/.local/bin/terragrunt
+mv terragrunt terragrunt_"${OLDVERSION:1}"
+mv tg-update/tg_"$VERSION" ~/.local/bin/terragrunt
 
 chmod +x ~/.local/bin/terragrunt
 
