d4aa4ea74af93ef9d0511c152f4c81976787ae4b
Flo Hessling <f.hessling@shopware.com>
2024-04-27 12:40:47 +0200

    fix: disable shellcheck in bashls to remove duplicate diagnostics

--- apps/nvim/lua/plugins/lang/shell.lua
+++ apps/nvim/lua/plugins/lang/shell.lua
@@ -1,35 +1,50 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, {
-                "bash",
-            })
-        end,
-    },
-    {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, {
-                "shellcheck",
-                "shfmt",
-            })
-        end,
-    },
-    {
-        "stevearc/conform.nvim",
-        opts = function(_, opts)
-            opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
-                sh = { "shfmt" },
-            })
-        end,
-    },
-    {
-        "mfussenegger/nvim-lint",
-        opts = function(_, opts)
-            opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
-                sh = { "shellcheck" },
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"bash",
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"shellcheck",
+				"shfmt",
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				bashls = {
+					settings = {
+						bashIde = {
+							-- disable shellcheck because it conflicts with linter
+							shellcheckPath = "",
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = function(_, opts)
+			opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+				sh = { "shfmt" },
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		opts = function(_, opts)
+			opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
+				sh = { "shellcheck" },
+			})
+		end,
+	},
 }
