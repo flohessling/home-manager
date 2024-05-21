11d65527d2cac5f5fe4fc727059e13b74093201d
Flo Hessling <f.hessling@shopware.com>
2024-05-21 09:29:09 +0200

    add harpoon, update nvim plugins

--- apps/nvim/lua/plugins/editor.lua
+++ apps/nvim/lua/plugins/editor.lua
@@ -41,6 +41,22 @@ return {
 		},
 	},
 	{
+		"ThePrimeagen/harpoon",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		event = "VeryLazy",
+		keys = {
+            -- stylua: ignore start
+            { "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon menu" },
+            { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Add file to Harpoon" },
+            { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Navigate to Harpoon file 1" },
+            { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Navigate to Harpoon file 2" },
+            { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Navigate to Harpoon file 3" },
+            { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Navigate to Harpoon file 4" },
+            { "<leader>5", function() require("harpoon.ui").nav_file(5) end, desc = "Navigate to Harpoon file 5" },
+			-- stylua: ignore end
+		},
+	},
+	{
 		"f-person/git-blame.nvim",
 		event = "VeryLazy",
 		init = function()
